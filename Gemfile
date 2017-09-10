53dc5438ea490a683c3cd4e2fcdb4c223d1e6198
Matthew Sullivan <matthew@msull92.com>
2017-09-10 12:23:00 -0500

    Add s3 functionality

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source "https://rubygems.org"
 
+# use aws-sdk to connect to s3 endpoint
+gem "aws-sdk", "~>2"
+
 # use sinatra to make a simple web server
 gem "sinatra"
--- main.rb
+++ main.rb
@@ -1,5 +1,21 @@
+require "aws-sdk"
+require "open-uri"
 require "sinatra"
 
-get "/webhook" do
+s3 = Aws::S3::Client.new(
+  access_key_id: ENV["S3_ACCESS_KEY_ID"],
+  secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
+  endpoint: ENV["S3_ENDPOINT"],
+  region: "none"
+)
+
+post "/" do
+  media_url = params["MediaUrl0"]
+  object_key = Time.now.to_i
+  object_body = open(media_url)
+
+  obj = s3.bucket("#{ENV["S3_BUCKET_NAME"]}").object(object_key)
+  obj.put(body: object_body)
+
   return 200
 end
