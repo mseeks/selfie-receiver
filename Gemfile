eba536744d6715d667a88a2b233226bda83597d4
Matthew Sullivan <matthew@msull92.com>
2017-09-28 14:17:28 -0500

    Write files to disk instead of to DO S3

--- Gemfile
+++ Gemfile
@@ -1,8 +1,5 @@
 source "https://rubygems.org"
 
-# use aws-sdk to connect to s3 endpoint
-gem "aws-sdk", "~>2"
-
 # use puma as the webserver hosting sinatra
 gem "puma"
 
--- main.rb
+++ main.rb
@@ -1,27 +1,18 @@
-require "aws-sdk"
 require "open-uri"
 require "sinatra"
 
 set :bind, "0.0.0.0"
 set :port, 80
 
-s3 = Aws::S3::Client.new(
-  access_key_id: ENV["S3_ACCESS_KEY_ID"],
-  secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
-  endpoint: ENV["S3_ENDPOINT"],
-  region: "none"
-)
-
 post "/" do
   media_url = params["MediaUrl0"]
   object_key = Time.now.to_i.to_s
-  object_body = open(media_url)
 
-  s3.put_object({
-    body: object_body,
-    bucket: ENV["S3_BUCKET_NAME"],
-    key: object_key,
-  })
+  open("/data/#{object_key}", "wb") do |file|
+    open(media_url) do |media|
+     file.write(media.read)
+    end
+  end
 
   return 200
 end
